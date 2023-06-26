f5631181a76f7092ff7ed7d405d437c4cb4b72aa
gabefo <gabrielfomiranda@gmail.com>
2023-06-26 02:08:18 -0300

    feat: create emoji picker

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, KeyboardEvent, useMemo, useRef, useState } from 'react'
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined'
+import KeyboardIcon from '@mui/icons-material/Keyboard'
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice'
 import MicIcon from '@mui/icons-material/Mic'
 import SendIcon from '@mui/icons-material/Send'
@@ -11,35 +12,27 @@ import IconButton from '@mui/material/IconButton'
 import InputBase from '@mui/material/InputBase'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import { alpha, styled } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import useVoiceRecorder from '@/hooks/useVoiceRecorder'
 import { IMessage, MessageType } from '@/interfaces'
 import formatDuration from '@/utils/formatDuration'
+import EmojiPicker from './EmojiPicker'
 
 const StyledInput = styled(InputBase)(({ theme }) => ({
   flex: 1,
   margin: theme.spacing(0, 1),
   padding: '8.5px 16px',
   borderRadius: '20px',
-  backgroundColor:
-    theme.palette.mode === 'light'
-      ? alpha(theme.palette.common.black, 0.06)
-      : alpha(theme.palette.common.white, 0.09),
+  backgroundColor: theme.vars.palette.FilledInput.bg,
   transition: theme.transitions.create('background-color', {
     easing: theme.transitions.easing.easeOut,
     duration: theme.transitions.duration.shorter,
   }),
   '&:hover': {
-    backgroundColor:
-      theme.palette.mode === 'light'
-        ? alpha(theme.palette.common.black, 0.09)
-        : alpha(theme.palette.common.white, 0.13),
+    backgroundColor: theme.vars.palette.FilledInput.hoverBg,
   },
   '&.Mui-focused': {
-    backgroundColor:
-      theme.palette.mode === 'light'
-        ? alpha(theme.palette.common.black, 0.06)
-        : alpha(theme.palette.common.white, 0.09),
+    backgroundColor: theme.vars.palette.FilledInput.bg,
   },
 }))
 
@@ -50,8 +43,13 @@ interface ChatInputProps {
 export default function ChatInput({ onSend }: ChatInputProps) {
   const [message, setMessage] = useState('')
   const isEmpty = useMemo(() => message.trim().length === 0, [message])
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false)
   const fileInputRef = useRef<HTMLInputElement>(null)
   const { recordingState, recordingTime, startRecording, stopRecording } = useVoiceRecorder()
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker((prev) => !prev)
+  }
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = event.target
@@ -91,69 +89,69 @@ export default function ChatInput({ onSend }: ChatInputProps) {
   }
 
   return (
-    <Container maxWidth={false}>
-      <Stack direction="row" alignItems="flex-end" sx={{ py: { xs: 1, sm: 2 } }}>
-        {recordingState === 'inactive' ? (
-          <>
-            <IconButton edge="start">
-              <EmojiEmotionsOutlinedIcon />
-            </IconButton>
-            <IconButton onClick={handleAttach}>
-              <AddPhotoAlternateOutlinedIcon />
-            </IconButton>
-            <StyledInput
-              autoFocus
-              multiline
-              maxRows={4}
-              value={message}
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
-              placeholder="Text message"
-            />
-            <input
-              ref={fileInputRef}
-              type="file"
-              accept="image/png, image/jpeg"
-              style={{ display: 'none' }}
-            />
-          </>
-        ) : (
-          <>
-            <IconButton edge="start" onClick={stopRecording}>
-              <DeleteIcon />
-            </IconButton>
-            <Stack
-              direction="row"
-              alignItems="center"
-              sx={{
-                flex: 1,
-                px: 2,
-                mx: 1,
-                borderRadius: '20px',
-                bgcolor: (theme) =>
-                  theme.palette.mode === 'light'
-                    ? alpha(theme.palette.common.black, 0.06)
-                    : alpha(theme.palette.common.white, 0.09),
-                userSelect: 'none',
-              }}
-            >
-              <Box sx={{ display: 'inline-flex', p: 1, ml: -1.5 }}>
-                <MicIcon color="error" />
-              </Box>
-              <Typography sx={{ flexGrow: 1 }}>{formatDuration(recordingTime)}</Typography>
-            </Stack>
-          </>
-        )}
-        <IconButton
-          edge="end"
-          onClick={isEmpty ? handleRecord : handleSend}
-          onPointerDown={(event) => {
-            event.preventDefault()
-          }}
-        >
-          {isEmpty && recordingState === 'inactive' ? <KeyboardVoiceIcon /> : <SendIcon />}
-        </IconButton>
-      </Stack>
-    </Container>
+    <>
+      <Container maxWidth={false}>
+        <Stack direction="row" alignItems="flex-end" sx={{ py: { xs: 1, sm: 2 } }}>
+          {recordingState === 'inactive' ? (
+            <>
+              <IconButton edge="start" onClick={toggleEmojiPicker}>
+                {showEmojiPicker ? <KeyboardIcon /> : <EmojiEmotionsOutlinedIcon />}
+              </IconButton>
+              <IconButton onClick={handleAttach}>
+                <AddPhotoAlternateOutlinedIcon />
+              </IconButton>
+              <StyledInput
+                autoFocus
+                multiline
+                maxRows={4}
+                value={message}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                placeholder="Text message"
+              />
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/png, image/jpeg"
+                style={{ display: 'none' }}
+              />
+            </>
+          ) : (
+            <>
+              <IconButton edge="start" onClick={stopRecording}>
+                <DeleteIcon />
+              </IconButton>
+              <Stack
+                direction="row"
+                alignItems="center"
+                sx={(theme) => ({
+                  flex: 1,
+                  px: 2,
+                  mx: 1,
+                  borderRadius: '20px',
+                  bgcolor: theme.vars.palette.FilledInput.bg,
+                  userSelect: 'none',
+                })}
+              >
+                <Box sx={{ display: 'inline-flex', p: 1, ml: -1.5 }}>
+                  <MicIcon color="error" />
+                </Box>
+                <Typography sx={{ flexGrow: 1 }}>{formatDuration(recordingTime)}</Typography>
+              </Stack>
+            </>
+          )}
+          <IconButton
+            edge="end"
+            onClick={isEmpty ? handleRecord : handleSend}
+            onPointerDown={(event) => {
+              event.preventDefault()
+            }}
+          >
+            {isEmpty && recordingState === 'inactive' ? <KeyboardVoiceIcon /> : <SendIcon />}
+          </IconButton>
+        </Stack>
+      </Container>
+      {showEmojiPicker && <EmojiPicker />}
+    </>
   )
 }
