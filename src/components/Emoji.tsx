23818424eba6dd90dc02b30d94c38af7ffe38232
gabefo <gabrielfomiranda@gmail.com>
2023-06-30 20:03:36 -0300

    fix: use img element to allow text selection

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -15,6 +15,7 @@ export default function Emoji({ emoji: emojiProp, size = sheet_size }: EmojiProp
     return null
   }
 
+  const native = String.fromCodePoint(...emoji.unified.split('-').map((s) => parseInt(s, 16)))
   const { sheet_x, sheet_y } = emoji
 
   const ratio = size / sheet_size
@@ -22,15 +23,21 @@ export default function Emoji({ emoji: emojiProp, size = sheet_size }: EmojiProp
   const y = -(sheet_y * (sheet_size + 2) + 1) * ratio
 
   return (
-    <span
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+      alt={native}
+      draggable={false}
       style={{
         display: 'inline-block',
         width: size,
         height: size,
+        border: 0,
         verticalAlign: 'top',
         backgroundImage: `url(${sheet.src})`,
         backgroundSize: `${sheet.width * ratio}px ${sheet.height * ratio}px`,
         backgroundPosition: `${x}px ${y}px`,
+        pointerEvents: 'none',
       }}
     />
   )
