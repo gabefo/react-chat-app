c7e03a01e699af59dedda16189759b15bd86ef48
gabefo <gabrielfomiranda@gmail.com>
2023-07-01 13:11:21 -0300

    fix: update regex

--- src/components/TextEmoji.tsx
+++ src/components/TextEmoji.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 import Emoji from './Emoji'
 
 const emojiRegex =
-  /\p{RI}\p{RI}|\p{Emoji}(\p{EMod}+|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u{200D}\p{Emoji}(\p{EMod}+|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?)+|\p{EPres}(\p{EMod}+|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?|\p{Emoji}(\p{EMod}+|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})/gu
+  /\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u{200D}(\p{RI}\p{RI}|\p{Emoji}(\p{EMod}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?))*/gu
 
 function renderEmojis(text: string) {
   const result: (string | ReactElement)[] = []
