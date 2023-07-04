39d12a5821fb92890b3a12d82f3331b81792e2ed
gabefo <gabrielfomiranda@gmail.com>
2023-07-04 00:41:40 -0300

    style: update layout

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,11 +4,7 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Alert from '@mui/material/Alert'
 import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Checkbox from '@mui/material/Checkbox'
 import Container from '@mui/material/Container'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Grid from '@mui/material/Grid'
 import IconButton from '@mui/material/IconButton'
 import InputAdornment from '@mui/material/InputAdornment'
 import Link from '@mui/material/Link'
@@ -20,18 +16,17 @@ import { useRouter } from 'next/router'
 import { signIn } from 'next-auth/react'
 import TitleAndMetaTags from '@/components/TitleAndMetaTags'
 
-const Root = styled('div')({
+const Root = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
+  position: 'relative',
   minHeight: '100%',
-})
-
-const StyledCard = styled(Card)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  padding: theme.spacing(4),
+  [theme.getColorSchemeSelector('light')]: {
+    backgroundColor: theme.vars.palette.common.white,
+  },
+  [theme.breakpoints.up('sm')]: {
+    justifyContent: 'center',
+  },
 }))
 
 const SignInPage: NextPage = () => {
@@ -73,78 +68,67 @@ const SignInPage: NextPage = () => {
   return (
     <Root>
       <TitleAndMetaTags title="Sign in" />
-      <Container maxWidth="xs">
-        <StyledCard>
+      <Container maxWidth="xs" disableGutters>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ p: 3 }}>
           <Typography component="h1" variant="h5" sx={{ textAlign: 'center', mb: 3 }}>
             Sign in
           </Typography>
           <Alert severity="info">
             Username: <strong>demo</strong> / Password: <strong>demo1234</strong>
           </Alert>
-          <Box component="form" onSubmit={handleSubmit} noValidate>
-            <TextField
-              variant="standard"
-              margin="normal"
-              fullWidth
-              error={error}
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              variant="standard"
-              margin="normal"
-              fullWidth
-              name="password"
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              error={error}
-              id="password"
-              autoComplete="current-password"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <LoadingButton
-              type="submit"
-              fullWidth
-              loading={isValidating}
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              <span>Sign In</span>
-            </LoadingButton>
-          </Box>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </StyledCard>
+          <TextField
+            margin="normal"
+            fullWidth
+            error={error}
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            error={error}
+            id="password"
+            autoComplete="current-password"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Link href="#" variant="body2" underline="none">
+            Forgot password?
+          </Link>
+          <LoadingButton
+            type="submit"
+            fullWidth
+            loading={isValidating}
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            <span>Sign In</span>
+          </LoadingButton>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            {"Don't have an account? "}
+            <Link href="#" variant="body2" underline="none">
+              Sign Up
+            </Link>
+          </Typography>
+        </Box>
       </Container>
     </Root>
   )
