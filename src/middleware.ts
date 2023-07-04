1dc3e322638052a6aac95c42372456b8c81416ad
gabefo <gabrielfomiranda@gmail.com>
2023-07-04 18:48:10 -0300

    fix: set config matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,3 +5,17 @@ export default withAuth({
     signIn: '/auth/signin',
   },
 })
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - auth (authentication routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|auth|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
